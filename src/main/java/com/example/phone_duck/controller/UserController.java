cac6b9bb583f79e04169c26efddb1f72e409f160
inoati1991@gmail.com <inoati1991@gmail.com>
2024-06-24 16:35:32 +0200

    Added user controller

---
+++ src/main/java/com/example/phone_duck/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.example.phone_duck.controller;
+
+import com.example.phone_duck.entity.User;
+import com.example.phone_duck.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        User newUser = userService.registerUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<User> loginUser(@RequestBody User user) {
+        Optional<User> loggedInUser = userService.loginUser(user.getUsername(), user.getPassword());
+        return loggedInUser.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+    }
+}

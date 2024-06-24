b8e8dd74d10becaf80ead911f169cb35cbb3b20c
inoati1991@gmail.com <inoati1991@gmail.com>
2024-06-24 14:37:08 +0200

    Added message entity

---
+++ src/main/java/com/example/phone_duck/entity/Message.java
@@ -0,0 +1,65 @@
+package com.example.phone_duck.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "channel_id")
+    private Channel channel;
+
+    // Constructors
+
+    public Message() {
+    }
+
+    public Message(String content, User user, Channel channel) {
+        this.content = content;
+        this.user = user;
+        this.channel = channel;
+    }
+
+    // Getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Channel getChannel() {
+        return channel;
+    }
+
+    public void setChannel(Channel channel) {
+        this.channel = channel;
+    }
+}
